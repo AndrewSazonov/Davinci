07a9d37ebb2403c0e451b0fbbed579274914a3aa
AndrewSazonov <sazonov@mail.org>
2017-12-18 16:51:02 +0100

    Fix style

--- Scripts/changelog.py
+++ Scripts/changelog.py
@@ -55,11 +55,13 @@ class Changelog:
                  date="2017-12-09",
                  list=['First release'])
 
-        #self.add(version="1.0.1",
-        #         date="2017-12-15",
-        #         list=['Adds support of the new input format: HEiDi at MLZ instrument log data',
-        #               'Fixes an issue with automatic determination of the peak and background parameters',
-        #               'Fixes a bug with cursor position change in the input data viewer when go through the scans'])
+        self.add(version="1.0.1",
+                 date="2017-12-18",
+                 list=['Adds support of the new input format: HEiDi at MLZ instrument log data',
+                       'Fixes an issue with automatic determination of the peak and background parameters',
+                       'Fixes a bug with cursor position change in the input data viewer when go through the scans',
+                       'Fixes a crash when open a new file',
+                       'Fixes a sidebar issue with the tabs width'])
 
         #self.add(version="1.0.2",
         #         date="2017-??-??",
