c96a6d14bf467e4db46c27b60ead2a8705c8979b
AndrewSazonov <mail@sazonov.org>
2018-08-09 06:49:57 +0200

    Config beautifiers

--- Libraries/Original/Widgets/Label.cpp
+++ Libraries/Original/Widgets/Label.cpp
@@ -1,21 +1,21 @@
 /*
- * Davinci, a software for the single-crystal diffraction data reduction.
- * Copyright (C) 2015-2017 Andrew Sazonov
- *
- * This file is part of Davinci.
- *
- * Davinci is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Davinci is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Davinci.  If not, see <http://www.gnu.org/licenses/>.
+    Davinci, a software for the single-crystal diffraction data reduction.
+    Copyright (C) 2015-2017 Andrew Sazonov
+
+    This file is part of Davinci.
+
+    Davinci is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Davinci is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Davinci.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "Macros.hpp"
@@ -23,24 +23,79 @@
 #include "Label.hpp"
 
 /*!
-\class As::Label
+   \class As::Label
 
-\brief The Label widget is a custom class based on the QLabel.
+   \brief The Label widget is a custom class based on the QLabel.
 
-\inmodule Widgets
-\ingroup Widgets
-*/
-
-/*!
-Constructs a label with the given \a parent.
-*/
-As::Label::Label(QWidget *parent)
-    : QLabel(parent) {}
+   \inmodule Widgets
+   \ingroup Widgets
+ */
 
 /*!
-Destroys the widget.
-*/
-As::Label::~Label()
-{
-    ADESTROYED;
+    Constructs a label with the given \a parent.
+ */
+As::Label::Label(QWidget* parent)
+  : QLabel(parent) {
 }
+
+/*!
+    Destroys the widget.
+ */
+As::Label::~Label  ( )   {
+}
+
+//fff
+
+/// ggg
+///
+
+//      m
+
+/* comment line 1
+   comment line 2 */
+
+/*!
+    comment line 1
+    comment line 2
+ */
+
+/*
+ * comment line 1
+ * comment line 2
+ */
+
+int Foo(bool isBar) {
+  char* foo1;
+  char& foo2;
+  string^ s1;
+
+  if (a) {
+    b;
+    c;
+  }
+
+  // hrthr
+  if (isBar) {
+    char* a = &b;
+    // gfbgbg
+    bar()
+
+
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+class Foo {
+public:
+Foo();
+virtual ~Foo();
+};
+
+class Foo {
+public:
+Foo();
+virtual ~Foo();
+};
