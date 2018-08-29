37e3754ef87119d58a442f64e360e40f9e7bdf71
AndrewSazonov <asazonov@asmac.local>
2018-08-29 11:45:13 +0200

    Add new test cases

--- Tests/DataTypes/TestRealMatrix9/TestRealMatrix9.cpp
+++ Tests/DataTypes/TestRealMatrix9/TestRealMatrix9.cpp
@@ -28,6 +28,15 @@ namespace As { //AS_BEGIN_NAMESPACE
 class TestRealMatrix9 : public QObject {
     Q_OBJECT
 
+    QVector<const char*> m_info;
+
+    QVector<RealMatrix9> m_matrices;
+
+    QVector<RealMatrix9> m_transposed;
+    QVector<RealMatrix9> m_inversed;
+
+    QVector<qreal> m_determinants;
+
   private slots:
     void initTestCase();
 
@@ -38,63 +47,61 @@ class TestRealMatrix9 : public QObject {
     void inv_data();
     void inv();
 
-  private:
-    QVector<const char*> dataNames;
-
-    QVector<RealMatrix9> matrices;
-
-    QVector<RealMatrix9> transposedMatrices;
-    QVector<RealMatrix9> inversedMatrices;
-
-    QVector<qreal> determinants;
-
 };
 
-void TestRealMatrix9::initTestCase() {
-    dataNames.append("identity matrix");
-    matrices.append(RealMatrix9( 1., 0., 0.,   0., 1., 0.,   0., 0., 1. ));
-    transposedMatrices.append(RealMatrix9( 1., 0., 0.,   0., 1., 0.,   0., 0., 1. ));
-    inversedMatrices.append(RealMatrix9( 1., 0., 0.,   0., 1., 0.,   0., 0., 1. ));
-    determinants.append(1.);
+// init
 
-    dataNames.append("random integer-like matrix with non-zero determinant");
-    matrices.append(RealMatrix9( 2., 1., -2.,   3., 2., 4.,   2., 1., -1. ));
-    transposedMatrices.append(RealMatrix9( 2., 3., 2.,   1., 2., 1.,   -2., 4., -1. ));
-    inversedMatrices.append(RealMatrix9( -6., -1., 8.,   11., 2., -14.,   -1., 0., 1. ));
-    determinants.append(1.); }
+void TestRealMatrix9::initTestCase() {
+    m_info.append("identity matrix");
+    m_matrices.append(RealMatrix9( 1., 0., 0.,   0., 1., 0.,   0., 0., 1. ));
+    m_transposed.append(RealMatrix9( 1., 0., 0.,   0., 1., 0.,   0., 0., 1. ));
+    m_inversed.append(RealMatrix9( 1., 0., 0.,   0., 1., 0.,   0., 0., 1. ));
+    m_determinants.append(1.);
+
+    m_info.append("random integer-like matrix with non-zero determinant");
+    m_matrices.append(RealMatrix9( 2., 1., -2.,   3., 2., 4.,   2., 1., -1. ));
+    m_transposed.append(RealMatrix9( 2., 3., 2.,   1., 2., 1.,   -2., 4., -1. ));
+    m_inversed.append(RealMatrix9( -6., -1., 8.,   11., 2., -14.,   -1., 0., 1. ));
+    m_determinants.append(1.); }
+
+// test next method
 
 void TestRealMatrix9::det_data() {
-    QTest::addColumn<RealMatrix9>("input");
-    QTest::addColumn<qreal>("result");
-    for (int i = 0; i < matrices.size(); ++i) {
-        QTest::newRow(dataNames[i]) << matrices[i] << determinants[i]; } }
+    QTest::addColumn<RealMatrix9>("matrix");
+    QTest::addColumn<qreal>("det");
+    for (int i = 0; i < m_matrices.size(); ++i) {
+        QTest::newRow(m_info[i]) << m_matrices[i] << m_determinants[i]; } }
 
 void TestRealMatrix9::det() {
-    QFETCH(RealMatrix9, input);
-    QFETCH(qreal, result);
-    QCOMPARE(input.det(), result); }
+    QFETCH(RealMatrix9, matrix);
+    QFETCH(qreal, det);
+    QCOMPARE(matrix.det(), det); }
+
+// test next method
 
 void TestRealMatrix9::trans_data() {
-    QTest::addColumn<RealMatrix9>("input");
-    QTest::addColumn<RealMatrix9>("result");
-    for (int i = 0; i < matrices.size(); ++i) {
-        QTest::newRow(dataNames[i]) << matrices[i] << transposedMatrices[i]; } }
+    QTest::addColumn<RealMatrix9>("matrix");
+    QTest::addColumn<RealMatrix9>("trans");
+    for (int i = 0; i < m_matrices.size(); ++i) {
+        QTest::newRow(m_info[i]) << m_matrices[i] << m_transposed[i]; } }
 
 void TestRealMatrix9::trans() {
-    QFETCH(RealMatrix9, input);
-    QFETCH(RealMatrix9, result);
-    QCOMPARE(input.trans(), result); }
+    QFETCH(RealMatrix9, matrix);
+    QFETCH(RealMatrix9, trans);
+    QCOMPARE(matrix.trans(), trans); }
+
+// test next method
 
 void TestRealMatrix9::inv_data() {
-    QTest::addColumn<RealMatrix9>("input");
-    QTest::addColumn<RealMatrix9>("result");
-    for (int i = 0; i < matrices.size(); ++i) {
-        QTest::newRow(dataNames[i]) << matrices[i] << inversedMatrices[i]; } }
+    QTest::addColumn<RealMatrix9>("matrix");
+    QTest::addColumn<RealMatrix9>("inv");
+    for (int i = 0; i < m_matrices.size(); ++i) {
+        QTest::newRow(m_info[i]) << m_matrices[i] << m_inversed[i]; } }
 
 void TestRealMatrix9::inv() {
-    QFETCH(RealMatrix9, input);
-    QFETCH(RealMatrix9, result);
-    QCOMPARE(input.inv(), result); }
+    QFETCH(RealMatrix9, matrix);
+    QFETCH(RealMatrix9, inv);
+    QCOMPARE(matrix.inv(), inv); }
 
 } //AS_END_NAMESPACE
 
--- Tests/DataTypes/TestRealVector/TestRealVector.cpp
+++ Tests/DataTypes/TestRealVector/TestRealVector.cpp
@@ -28,11 +28,11 @@ namespace As { //AS_BEGIN_NAMESPACE
 class TestRealVector : public QObject {
     Q_OBJECT
 
-    QVector<const char*> m_dataNames;
+    QVector<const char*> m_info;
 
-    QVector<As::RealVector> m_input;
+    QVector<As::RealVector> m_vectors;
 
-    QVector<As::RealVector> m_reverse;
+    QVector<As::RealVector> m_reversed;
 
     QVector<qreal> m_min;
     QVector<qreal> m_max;
@@ -47,17 +47,17 @@ class TestRealVector : public QObject {
 
     void initTestCase();
 
-    //void reverse_data();
-    //void reverse();
+    void reverse_data();
+    void reverse();
 
     void min_data();
     void min();
-    /*void max_data();
+    void max_data();
     void max();
     void sum_data();
     void sum();
-    void sumSqrt_data();
-    void sumSqrt();
+    void sumSqr_data();
+    void sumSqr();
     void mean_data();
     void mean();
     void range_data();
@@ -65,15 +65,17 @@ class TestRealVector : public QObject {
     void middle_data();
     void middle();
     void step_data();
-    void step();*/
+    void step();
 
 };
 
+// init
+
 void TestRealVector::initTestCase() {
-    m_dataNames.append("random integer-like vector with positive values only; from QVector<qreal>");
-    m_input.append(QVector<qreal>{ 2., 3., 1., 5., 4. });
-    m_reverse.append(QVector<qreal>{ 4., 5., 1., 3., 2. });
-    m_min.append(10.9999911111);
+    m_info.append("random integer-like vector with positive values only; from QVector<qreal>");
+    m_vectors.append(QVector<qreal> { 2., 3., 1., 5., 4. });
+    m_reversed.append(QVector<qreal> { 4., 5., 1., 3., 2. });
+    m_min.append(1.);
     m_max.append(5.);
     m_sum.append(15.);
     m_sumSqr.append(55.);
@@ -82,19 +84,146 @@ void TestRealVector::initTestCase() {
     m_middle.append(3.);
     m_step.append(1.);
 
- }
+    m_info.append("random integer-like vector with negative values only; from QString");
+    m_vectors.append(QString("-2. -3. -1. -5. -4."));
+    m_reversed.append(QString("-4. -5. -1. -3. -2."));
+    m_min.append(-5.);
+    m_max.append(-1.);
+    m_sum.append(-15.);
+    m_sumSqr.append(55.);
+    m_mean.append(-3.);
+    m_range.append(4.);
+    m_middle.append(-3.);
+    m_step.append(1.);
+
+    m_info.append("random vector with both positive and negative values; from QString");
+    m_vectors.append(QString("5.5 -3. 0. 5.5 -4. 0.5"));
+    m_reversed.append(QString("0.5 -4. 5.5 0. -3. 5.5"));
+    m_min.append(-4.);
+    m_max.append(5.5);
+    m_sum.append(4.5);
+    m_sumSqr.append(85.75);
+    m_mean.append(0.75);
+    m_range.append(9.5);
+    m_middle.append(0.75);
+    m_step.append(1.9); }
+
+// test next method
+
+void TestRealVector::reverse_data() {
+    QTest::addColumn<RealVector>("vector");
+    QTest::addColumn<RealVector>("reverse");
+    for (int i = 0; i < m_vectors.size(); ++i) {
+        QTest::newRow(m_info[i]) << m_vectors[i] << m_reversed[i]; } }
+
+void TestRealVector::reverse() {
+    QFETCH(RealVector, vector);
+    QFETCH(RealVector, reverse);
+    QCOMPARE(vector.reverse(), reverse); }
+
+// test next method
 
 void TestRealVector::min_data() {
-    QTest::addColumn<RealVector>("input");
-    QTest::addColumn<qreal>("result");
-    for (int i = 0; i < m_input.size(); ++i) {
-        QTest::newRow(m_dataNames[i]) << m_input[i] << m_min[i]; } }
+    QTest::addColumn<RealVector>("vector");
+    QTest::addColumn<qreal>("min");
+    for (int i = 0; i < m_vectors.size(); ++i) {
+        QTest::newRow(m_info[i]) << m_vectors[i] << m_min[i]; } }
 
 void TestRealVector::min() {
-    QFETCH(RealVector, input);
-    QFETCH(qreal, result);
-    QCOMPARE(input.min(), result); }
+    QFETCH(RealVector, vector);
+    QFETCH(qreal, min);
+    QCOMPARE(vector.min(), min); }
 
+// test next method
+
+void TestRealVector::max_data() {
+    QTest::addColumn<RealVector>("vector");
+    QTest::addColumn<qreal>("max");
+    for (int i = 0; i < m_vectors.size(); ++i) {
+        QTest::newRow(m_info[i]) << m_vectors[i] << m_max[i]; } }
+
+void TestRealVector::max() {
+    QFETCH(RealVector, vector);
+    QFETCH(qreal, max);
+    QCOMPARE(vector.max(), max); }
+
+// test next method
+
+void TestRealVector::sum_data() {
+    QTest::addColumn<RealVector>("vector");
+    QTest::addColumn<qreal>("sum");
+    for (int i = 0; i < m_vectors.size(); ++i) {
+        QTest::newRow(m_info[i]) << m_vectors[i] << m_sum[i]; } }
+
+void TestRealVector::sum() {
+    QFETCH(RealVector, vector);
+    QFETCH(qreal, sum);
+    QCOMPARE(vector.sum(), sum); }
+
+// test next method
+
+void TestRealVector::sumSqr_data() {
+    QTest::addColumn<RealVector>("vector");
+    QTest::addColumn<qreal>("sumSqr");
+    for (int i = 0; i < m_vectors.size(); ++i) {
+        QTest::newRow(m_info[i]) << m_vectors[i] << m_sumSqr[i]; } }
+
+void TestRealVector::sumSqr() {
+    QFETCH(RealVector, vector);
+    QFETCH(qreal, sumSqr);
+    QCOMPARE(vector.sumSqr(), sumSqr); }
+
+// test next method
+
+void TestRealVector::mean_data() {
+    QTest::addColumn<RealVector>("vector");
+    QTest::addColumn<qreal>("mean");
+    for (int i = 0; i < m_vectors.size(); ++i) {
+        QTest::newRow(m_info[i]) << m_vectors[i] << m_mean[i]; } }
+
+void TestRealVector::mean() {
+    QFETCH(RealVector, vector);
+    QFETCH(qreal, mean);
+    QCOMPARE(vector.mean(), mean); }
+
+// test next method
+
+void TestRealVector::range_data() {
+    QTest::addColumn<RealVector>("vector");
+    QTest::addColumn<qreal>("range");
+    for (int i = 0; i < m_vectors.size(); ++i) {
+        QTest::newRow(m_info[i]) << m_vectors[i] << m_range[i]; } }
+
+void TestRealVector::range() {
+    QFETCH(RealVector, vector);
+    QFETCH(qreal, range);
+    QCOMPARE(vector.range(), range); }
+
+// test next method
+
+void TestRealVector::middle_data() {
+    QTest::addColumn<RealVector>("vector");
+    QTest::addColumn<qreal>("middle");
+    for (int i = 0; i < m_vectors.size(); ++i) {
+        QTest::newRow(m_info[i]) << m_vectors[i] << m_middle[i]; } }
+
+void TestRealVector::middle() {
+    QFETCH(RealVector, vector);
+    QFETCH(qreal, middle);
+    QCOMPARE(vector.middle(), middle); }
+
+// test next method
+
+void TestRealVector::step_data() {
+    QTest::addColumn<RealVector>("vector");
+    QTest::addColumn<qreal>("step");
+    for (int i = 0; i < m_vectors.size(); ++i) {
+        QTest::newRow(m_info[i]) << m_vectors[i] << m_step[i]; } }
+
+void TestRealVector::step() {
+    QFETCH(RealVector, vector);
+    QFETCH(qreal, step);
+    QCOMPARE(vector.step(), step); }
 
 } //AS_END_NAMESPACE
 
